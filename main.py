a8b4be1922c6f30228e34fda9937079405fa7bc2
eshaan <eshaan2031@icloud.com>
2025-05-05 10:40:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
 import narwhal
 import sage
-import pickle
+import json
 import os
 import time
 
-DATA = "data.narl"
+os.system("clear")
+
+DATA = "data.json"
 
 # so this is basically gonna get input features from user and then use data from file to eval results
 
 action = input("What stock do you want to test? ")
 
 if os.path.exists(DATA):
-    with open(DATA, "rb") as f:
-        existing_data = pickle.load(f)
-    prediction = narwhal.narwhal([sage.get_price(action), sage.get_market_cap(action), sage.get_market_open(action), sage.get_time()], DATA)
-    print("NARwhaL prediction: " + prediction)
+    with open(DATA, "r") as f:
+        existing_data = json.load(f)
+    prediction = narwhal.narwhal([sage.get_price(action), sage.get_market_cap(action), sage.get_market_open(action), sage.get_time()], existing_data)
+    print("NARwhaL prediction: " + str(prediction))
     record = input("Do you want to record results? (y/n) ")
     if record == "y":
-        length = input("How long do you want to wait before recording (minutes)? ")
+        length = float(input("How long do you want to wait before recording (minutes)? "))
         time.sleep(length*60)
-        grade = input("How would you grade this stock out of 100? ")
+        grade = float(input("How would you grade this stock out of 100? "))
         existing_data.append([price, mktcap, mktopn, minsmid, prediction, grade])
-        with open(DATA, "wb") as f:
-            pickle.dump(existing_data, f)
+        with open(DATA, "w") as f:
+            json.dump(existing_data, f)
 else:
     log_stuff = input("No data found?? Do you want to log data? (y/n) ")
     if log_stuff == "y":
         num_logs = int(input("How many datapoints do you want to log? "))
         for i in range(num_logs):
-            price = input("Price: ")
-            mktcap = input("Market Cap: ")
-            mktopn = input("Market Open: ")
-            minsmid = input("Minutes Since Day Started (360 for 6:00 AM): ")
-            pred = input("How much was the prediction? ")
-            out = input("How would you grade the prediction accuracy out of 100? ")
+            price = float(input("Price: "))
+            mktcap = float(input("Market Cap: "))
+            mktopn = float(input("Market Open: "))
+            minsmid = float(input("Minutes Since Day Started (360 for 6:00 AM): "))
+            pred = float(input("How much was the prediction? "))
+            out = float(input("How would you grade the prediction accuracy out of 100? "))
             if os.path.exists(DATA):
-                with open(DATA, "rb") as f:
-                    existing_data = pickle.load(f)
+                with open(DATA, "r") as f:
+                    existing_data = json.load(f)
                 existing_data.append([price, mktcap, mktopn, minsmid, pred, out])
-                with open(DATA, "wb") as f:
-                    pickle.dump(existing_data, f)
+                with open(DATA, "w") as f:
+                    json.dump(existing_data, f)
             else:
-                with open(DATA, "wb") as f:
-                    pickle.dump([[price, mktcap, mktopn, minsmid, pred, out]], f)
-
-
-
+                with open(DATA, "w") as f:
+                    json.dump([[price, mktcap, mktopn, minsmid, pred, out]], f)
