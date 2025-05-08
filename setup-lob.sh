ec5151d7e8fbd62c0fe8d75dff0f744f9feffdbf
eshaan <eshaan2031@icloud.com>
2025-05-08 16:13:48 +0000

    a

--- setup-lob.sh
+++ setup-lob.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt upgrade && sudo apt install git wget
+sudo apt update && sudo apt upgrade && sudo apt install git wget ffmpeg
 bash -ci "$(wget -qO - 'https://shlink.makedeb.org/install')"
 wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
