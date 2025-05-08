83a0cdd842a00c511462ad2b4101b046a9758672
eshaan <eshaan2031@icloud.com>
2025-05-08 16:08:46 +0000

    a

---
+++ setup-lob.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade && sudo apt install git wget
+bash -ci "$(wget -qO - 'https://shlink.makedeb.org/install')"
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+sudo apt update && sudo apt install mist
+mist update && mist install lobster-git
